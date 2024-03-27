6ea05e9fa0933aba6dce070daaf86d706b60d567
Kostia <kostia3507@gmail.com>
2024-03-28 01:34:38 +0200

    added view for car serializer

--- car/views.py
+++ car/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from car.models import Car
+from car.serializers import CarSerializer
+
+
+class CarViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
