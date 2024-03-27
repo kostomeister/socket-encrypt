b24660f35a4f31ae03c3c79a6330788dc30f57ba
Kostia <kostia3507@gmail.com>
2024-03-27 20:12:37 +0200

    added car model

--- car/models.py
+++ car/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.color} - {self.brand}"
