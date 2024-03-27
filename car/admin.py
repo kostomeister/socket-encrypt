79d9d1230f3da24f524e1e6accc75dd9c2bad0c3
Kostia <kostia3507@gmail.com>
2024-03-28 00:53:25 +0200

    added admin panel for car model

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'brand')
+    list_filter = ('color', 'brand')
+    search_fields = ('name', 'brand')
+
+
+admin.site.register(Car, CarAdmin)
