5e04c27ed533da6d88640e1fb8307d05cb1aea1c
Kostia <kostia3507@gmail.com>
2024-03-28 02:30:41 +0200

    ran black

--- car/admin.py
+++ car/admin.py
@@ -3,9 +3,9 @@ from .models import Car
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('name', 'color', 'brand')
-    list_filter = ('color', 'brand')
-    search_fields = ('name', 'brand')
+    list_display = ("name", "color", "brand")
+    list_filter = ("color", "brand")
+    search_fields = ("name", "brand")
 
 
 admin.site.register(Car, CarAdmin)
--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cars/", include("car.urls", namespace="cars"))
+    path("api/cars/", include("car.urls", namespace="cars")),
 ]
